b46a4c784399461d11f812cdeef75dc487e7ce33
Christopher Dent <christopherdent01@gmail.com>
2019-09-22 23:39:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,5 +3,11 @@ require 'pry'
 
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry 
+
+
   project_hash = Hash.new
 end
+
