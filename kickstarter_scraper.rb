7b8143575144c5c070205d406fbb30b7e3b7cf4b
Christopher Dent <christopherdent01@gmail.com>
2019-09-23 00:48:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,7 +20,7 @@ create_project_hash
 #title:  project.css("h2.bbcard_name strong a").text 
 #image link:  project.css("div.project-thumbnail a img").attribute("src").value 
 #description:  project.css("p.bbcard_blurb").text 
-#location:  project.css("ul.project-meta li a span.location-name").text 
+#location:  project.css("li a span.location-name").text 
 
 #percent:  project.css("ul.project-stats li.first.funded strong").text
 
