ef02c22b598690dc3ed1ab9133a2cbc45e7a8777
Christopher Dent <christopherdent01@gmail.com>
2019-09-23 00:18:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,6 +16,8 @@ create_project_hash
 #title:  project.css("h2.bbcard_name strong a").text 
 #image link:  project.css("div.project-thumbnail a img").attribute("src").value 
 #description:  project.css("p.bbcard_blurb").text 
+#location:  project.css("li")
+
 
 <p class="bbcard_blurb">
 Four young composers join forces with an indie orchestra to present this musical reflection on a literary masterpiece.
